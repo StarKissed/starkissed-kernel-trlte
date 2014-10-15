60a82f494c08dfd39e1d76edab2f363937634f06
Ruchi Kandoi <kandoiruchi@google.com>
2014-10-14 17:43:21 -0700

    power: Avoids bogus error messages for the suspend aborts.
    Avoids printing bogus error message "tasks refusing to freeze", in
    cases where pending wakeup source caused the suspend abort.
    
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Change-Id: I913ad290f501b31cd536d039834c8d24c6f16928

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -89,31 +89,29 @@ static int try_to_freeze_tasks(bool user_only)
 	do_div(elapsed_msecs64, NSEC_PER_MSEC);
 	elapsed_msecs = elapsed_msecs64;
 
-	if (todo) {
+	if (wakeup) {
 		printk("\n");
-		printk(KERN_ERR "Freezing of tasks %s after %d.%03d seconds "
-		       "(%d tasks refusing to freeze, wq_busy=%d):\n",
-		       wakeup ? "aborted" : "failed",
+		printk(KERN_ERR "Freezing of tasks aborted after %d.%03d seconds",
+		       elapsed_msecs / 1000, elapsed_msecs % 1000);
+#ifdef CONFIG_SEC_PM_DEBUG
+            printk(KERN_ERR "Freezing of %s aborted (%d) (%s)\n",
+                   user_only ? "user space " : "tasks ",
+                   q ? q->pid : 0, q ? q->comm : "NONE");
+#endif
+	} else if (todo) {
+		printk("\n");
+		printk(KERN_ERR "Freezing of tasks failed after %d.%03d seconds"
+		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
 		       todo - wq_busy, wq_busy);
 
-#ifdef CONFIG_SEC_PM_DEBUG
-		if (wakeup) {
-			printk(KERN_ERR "Freezing of %s aborted (%d) (%s)\n",
-					user_only ? "user space " : "tasks ",
-					q ? q->pid : 0, q ? q->comm : "NONE");
-		}
-#endif
-
-		if (!wakeup) {
-			read_lock(&tasklist_lock);
-			do_each_thread(g, p) {
-				if (p != current && !freezer_should_skip(p)
-				    && freezing(p) && !frozen(p))
-					sched_show_task(p);
-			} while_each_thread(g, p);
-			read_unlock(&tasklist_lock);
-		}
+		read_lock(&tasklist_lock);
+		do_each_thread(g, p) {
+			if (p != current && !freezer_should_skip(p)
+			    && freezing(p) && !frozen(p))
+				sched_show_task(p);
+		} while_each_thread(g, p);
+		read_unlock(&tasklist_lock);
 	} else {
 		printk("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
 			elapsed_msecs % 1000);
