8e2c2cc68f962dbf5394e33d13433c0f2a64ed48
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-26 12:14:57 -0400

    CPU Overclock to 2.8, Underclock 192 [APQ8084]
    These values need confirmation, as they are only educated guesses based
    on existing values at this point.
    
    Also remove some unnecessary and rather arrogant credits for simply
    copying a frequency value into a definition.
    
    CPU Underclock 192 [APQ8084]
    
    While 192 can be inferred as the min from the dtsi config, the runtime
    configuration still lists 268 as the absolute minimum

--- include/linux/cpufreq_limit.h
+++ include/linux/cpufreq_limit.h
@@ -14,14 +14,8 @@ struct cpufreq_limit_handle;
 
 #ifdef CONFIG_CPU_FREQ_LIMIT
 
-//#if defined(CONFIG_ARCH_MSM8974PRO)
-//#define MAX_FREQ_LIMIT	2457600
-//#else
-//#define MAX_FREQ_LIMIT	2265600
-//#endif
-// Yank555.lu : let Samsung's CPUFreq Limiter know how high we can really go
-#define MAX_FREQ_LIMIT	2496000
-#define MIN_FREQ_LIMIT	300000
+#define MAX_FREQ_LIMIT	2803200
+#define MIN_FREQ_LIMIT	268000
 
 struct cpufreq_limit_handle *cpufreq_limit_get(unsigned long min_freq,
 		unsigned long max_freq, char *label);
