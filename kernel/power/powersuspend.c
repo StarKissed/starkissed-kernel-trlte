972c9e3521041d2e1a856d48a457dfb943ef45e4
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-26 23:00:44 -0400

    Remove line-by-line signatures, Update PS mode
    The header is the only signature required, not individually next to
    every comment. That is obsessive and leads to merge failures over
    unnecessary inclusions.

--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -45,8 +45,8 @@ static DECLARE_WORK(power_suspend_work, power_suspend);
 static DECLARE_WORK(power_resume_work, power_resume);
 static DEFINE_SPINLOCK(state_lock);
 
-static int state; // Yank555.lu : Current powersave state (screen on / off)
-static int mode;  // Yank555.lu : Current powersave mode  (kernel / userspace / panel / hybrid)
+static int state; // Current powersave state (screen on / off)
+static int mode;  // Current powersave mode  (kernel / userspace / panel / hybrid)
 
 void register_power_suspend(struct power_suspend *handler)
 {
@@ -162,7 +162,7 @@ void set_power_suspend_state_autosleep_hook(int new_state)
 	#ifdef POWER_SUSPEND_DEBUG
 	pr_info("[POWERSUSPEND] autosleep resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");
 	#endif
-	// Yank555.lu : Only allow autosleep hook changes in autosleep & hybrid mode
+	// Only allow autosleep hook changes in autosleep & hybrid mode
 	if (mode == POWER_SUSPEND_AUTOSLEEP || mode == POWER_SUSPEND_HYBRID)
 		set_power_suspend_state(new_state);
 }
@@ -174,7 +174,7 @@ void set_power_suspend_state_panel_hook(int new_state)
 	#ifdef POWER_SUSPEND_DEBUG
 	pr_info("[POWERSUSPEND] panel resquests %s.\n", new_state == POWER_SUSPEND_ACTIVE ? "sleep" : "wakeup");
 	#endif
-	// Yank555.lu : Only allow autosleep hook changes in autosleep & hybrid mode
+	// Only allow autosleep hook changes in autosleep & hybrid mode
 	if (mode == POWER_SUSPEND_PANEL || mode == POWER_SUSPEND_HYBRID)
 		set_power_suspend_state(new_state);
 }
@@ -194,7 +194,7 @@ static ssize_t power_suspend_state_store(struct kobject *kobj,
 {
 	int new_state = 0;
 
-	// Yank555.lu : Only allow sysfs changes from userspace mode
+	// Only allow sysfs changes from userspace mode
 	if (mode != POWER_SUSPEND_USERSPACE)
 		return -EINVAL;
 
@@ -298,10 +298,10 @@ static int __init power_suspend_init(void)
 		return -ENOMEM;
 	}
 
-//	mode = POWER_SUSPEND_AUTOSLEEP;	// Yank555.lu : Default to autosleep mode
-//	mode = POWER_SUSPEND_USERSPACE;	// Yank555.lu : Default to userspace mode
-//	mode = POWER_SUSPEND_PANEL;	// Yank555.lu : Default to display panel mode
-	mode = POWER_SUSPEND_HYBRID;	// Yank555.lu : Default to display panel / autosleep hybrid mode
+//	mode = POWER_SUSPEND_AUTOSLEEP;	// Default to autosleep mode
+//	mode = POWER_SUSPEND_USERSPACE;	// Default to userspace mode
+	mode = POWER_SUSPEND_PANEL;	// Default to display panel mode
+//	mode = POWER_SUSPEND_HYBRID;	// Default to display panel / autosleep hybrid mode
 
 	return 0;
 }
