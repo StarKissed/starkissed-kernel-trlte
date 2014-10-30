7168a6fa829e4c0bdc1c99eb703c5639868af2ff
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-30 16:02:21 -0400

    Build: Add support for additional carriers Update for add'l carries, Add Sprint ramdisk
    Update permissions for existing configurations
    
    Cleaner classification for handling add'l carriers

--- buildKernel.sh
+++ buildKernel.sh
@@ -4,50 +4,76 @@
 
 # This script is designed by Twisted Playground for use on MacOSX 10.7 but can be modified for other distributions of Mac and Linux
 
-PROPER=`echo $1 | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g'`
-
 HANDLE=LoungeKatt
-KERNELSPEC=/Volumes/android/trltetmo-kernel
+KERNELSPEC=/Volumes/android/starkissed-kernel-trlte
 KERNELREPO=$DROPBOX_SERVER/TwistedServer/Playground/kernels
 TOOLCHAIN_PREFIX=/Volumes/android/android-toolchain-eabi-4.7/bin/arm-eabi-
-MODULEOUT=$KERNELSPEC/buildimg/boot.img-ramdisk
-KERNELHOST=public_html/trltetmo/kernel
-GOOSERVER=upload.goo.im:$KERNELHOST
 PUNCHCARD=`date "+%m-%d-%Y_%H.%M"`
 
-LOCALZIP=$HANDLE"_StarKissed-NJ7-Note4.zip"
+echo "1. T-Mo"
+echo "2. AT&T"
+echo "3. VZW"
+echo "4. Spr"
+echo "Please Choose: "
+read profile
+
+case $profile in
+1)
+TYPE=tmo
+BUILD=NJ7
+;;
+2)
+TYPE=att
+BUILD=NA
+;;
+3)
+TYPE=vzw
+BUILD=NA
+;;
+4)
+TYPE=spr
+BUILD=NIE
+;;
+*)
+exit 1
+;;
+esac
+
+PROPER=`echo $TYPE | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g'`
+MODULEOUT=$KERNELSPEC/build`echo $TYPE`/boot.img-ramdisk
+KERNELHOST=public_html/trlte`echo $TYPE`/kernel
+GOOSERVER=upload.goo.im:$KERNELHOST
 IMAGEFILE=boot.$PUNCHCARD.img
 KERNELFILE=boot.$PUNCHCARD.tar
-KERNELZIP=StarKissed-NJ7_$PUNCHCARD-Note4.zip
+LOCALZIP=$HANDLE"_StarKissed-trlte["`echo $TYPE`"."`echo $BUILD`"].zip"
+KERNELZIP="StarKissed-"$PUNCHCARD"-trlte["`echo $TYPE`"."`echo $BUILD`"].zip"
 
 CPU_JOB_NUM=8
 
-if [ -e $KERNELSPEC/buildimg/boot.img ]; then
-    rm -R $KERNELSPEC/buildimg/boot.img
+if [ -e $KERNELSPEC/build`echo $TYPE`/boot.img ]; then
+    rm -R $KERNELSPEC/build`echo $TYPE`/boot.img
 fi
-if [ -e $KERNELSPEC/buildimg/newramdisk.cpio.gz ]; then
-    rm -R $KERNELSPEC/buildimg/newramdisk.cpio.gz
+if [ -e $KERNELSPEC/build`echo $TYPE`/newramdisk.cpio.gz ]; then
+    rm -R $KERNELSPEC/build`echo $TYPE`/newramdisk.cpio.gz
 fi
-if [ -e $KERNELSPEC/buildimg/zImage ]; then
-    rm -R $KERNELSPEC/buildimg/zImage
+if [ -e $KERNELSPEC/build`echo $TYPE`/zImage ]; then
+    rm -R $KERNELSPEC/build`echo $TYPE`/zImage
 fi
 if [ -e arch/arm/boot/zImage ]; then
     rm -R arch/arm/boot/zImage
 fi
-if [ -e $KERNELSPEC/trltetmoSKU/$LOCALZIP ];then
-    rm -R $KERNELSPEC/trltetmoSKU/$LOCALZIP
+if [ -e $KERNELSPEC/trlteSKU/$LOCALZIP ];then
+    rm -R $KERNELSPEC/trlteSKU/$LOCALZIP
 fi
 
-cp -R config/apq8084_sec_trlte_tmo_defconfig  arch/arm/configs/apq8084_sec_trlte_tmo_defconfig
+cp -R config/apq8084_sec_trlte_`echo $TYPE`_defconfig arch/arm/configs/apq8084_sec_trlte_`echo $TYPE`_defconfig
 cp -R config/apq8084_sec_defconfig  arch/arm/configs/apq8084_sec_defconfig
 
 make -j$CPU_JOB_NUM -C $(pwd) clean
-make -j$CPU_JOB_NUM -C $(pwd) VARIANT_DEFCONFIG=apq8084_sec_trlte_tmo_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig CROSS_COMPILE=$TOOLCHAIN_PREFIX
+make -j$CPU_JOB_NUM -C $(pwd) VARIANT_DEFCONFIG=apq8084_sec_trlte_`echo $TYPE`_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig CROSS_COMPILE=$TOOLCHAIN_PREFIX
 make -j$CPU_JOB_NUM -C $(pwd) CROSS_COMPILE=$TOOLCHAIN_PREFIX
 
 if [ -e arch/arm/boot/zImage ]; then
-
-    MODULEOUT=$KERNELSPEC/buildimg/boot.img-ramdisk
 
     if [ `find . -name "*.ko" | grep -c ko` > 0 ]; then
 
@@ -72,9 +98,9 @@ if [ -e arch/arm/boot/zImage ]; then
 
     fi
 
-    cp -R arch/arm/boot/zImage $KERNELSPEC/buildimg/zImage
+    cp -R arch/arm/boot/zImage build`echo $TYPE`
 
-    cd buildimg
+    cd build`echo $TYPE`
     ./img.sh
 
     echo "building boot package"
@@ -91,7 +117,7 @@ if [ -e arch/arm/boot/zImage ]; then
         rm -R output/boot.tar.md5.gz
     fi
 
-    cp -r  output/boot.img $KERNELREPO/trltetmo/boot.img
+    cp -r  output/boot.img $KERNELREPO/trlte`echo $TYPE`/boot.img
 
 #    if cat /etc/issue | grep Ubuntu; then
 #        tar -H ustar -c output/boot.img > output/boot.tar
@@ -99,7 +125,7 @@ if [ -e arch/arm/boot/zImage ]; then
 #        tar --format ustar -c output/boot.img > output/boot.tar
 #    fi
     tar cvf output/boot.tar output/boot.img
-    cp -r output/boot.tar $KERNELREPO/trltetmo/boot.tar
+    cp -r output/boot.tar $KERNELREPO/trlte`echo $TYPE`/boot.tar
     cp -r output/boot.tar output/boot.tar.md5
     if cat /etc/issue | grep Ubuntu; then
         md5sum -t output/boot.tar.md5 >> output/boot.tar.md5
@@ -107,13 +133,13 @@ if [ -e arch/arm/boot/zImage ]; then
         md5 -r output/boot.tar.md5 >> output/boot.tar.md5
     fi
 
-    cp -r output/boot.tar.md5 $KERNELREPO/trltetmo/boot.tar.md5
-    cp -R output/boot.img trltetmoSKU
-    cd trltetmoSKU
+    cp -r output/boot.tar.md5 $KERNELREPO/trlte`echo $TYPE`/boot.tar.md5
+    cp -R output/boot.img trlteSKU
+    cd trlteSKU
     rm *.zip
     zip -r $LOCALZIP *
     cd ../
-    cp -R $KERNELSPEC/trltetmoSKU/$LOCALZIP $KERNELREPO/$LOCALZIP
+    cp -R $KERNELSPEC/trlteSKU/$LOCALZIP $KERNELREPO/$LOCALZIP
 
     echo "Publish Kernel?"
     read publish
@@ -125,16 +151,16 @@ if [ -e arch/arm/boot/zImage ]; then
             rm -R $KERNELREPO/gooserver/*.md5
             rm -R $KERNELREPO/gooserver/*.zip
         fi
-        cp -r  $KERNELREPO/trltetmo/boot.img $KERNELREPO/gooserver/$IMAGEFILE
+        cp -r  $KERNELREPO/trlte`echo $TYPE`/boot.img $KERNELREPO/gooserver/$IMAGEFILE
         ssh upload.goo.im mv -f $KERNELHOST/*.img $KERNELHOST/archive/
         scp $KERNELREPO/gooserver/$IMAGEFILE $GOOSERVER
-        cp -r $KERNELREPO/trltetmo/boot.tar $KERNELREPO/gooserver/$KERNELFILE
+        cp -r $KERNELREPO/trlte`echo $TYPE`/boot.tar $KERNELREPO/gooserver/$KERNELFILE
         ssh upload.goo.im mv -f $KERNELHOST/*.tar $KERNELHOST/archive/
         scp $KERNELREPO/gooserver/$KERNELFILE $GOOSERVER/
-        cp -r $KERNELREPO/trltetmo/boot.tar.md5 $KERNELREPO/gooserver/$KERNELFILE.md5
+        cp -r $KERNELREPO/trlte`echo $TYPE`/boot.tar.md5 $KERNELREPO/gooserver/$KERNELFILE.md5
         ssh upload.goo.im mv -f $KERNELHOST/*.md5 $KERNELHOST/archive/
         scp $KERNELREPO/gooserver/$KERNELFILE.md5 $GOOSERVER
-        cp -r $KERNELREPO/$LOCALZIP $KERNELREPO/gooserver/$KERNELZIP
+        cp -r $KERNELREPO/$LOCALZIP $KERNELREPO/gooserver/`echo $KERNELZIP`
         ssh upload.goo.im mv -f $KERNELHOST/*.zip $KERNELHOST/archive/
         scp $KERNELREPO/gooserver/$KENRELZIP $GOOSERVER
     fi
