a59b0016f63cb7d30cd21e306259afab268a364f
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-29 22:55:10 -0400

    Consistent default limits across the board ;)

--- include/linux/cpufreq_limit.h
+++ include/linux/cpufreq_limit.h
@@ -14,7 +14,7 @@ struct cpufreq_limit_handle;
 
 #ifdef CONFIG_CPU_FREQ_LIMIT
 
-#define MAX_FREQ_LIMIT	2803200
+#define MAX_FREQ_LIMIT	2649600
 #define MIN_FREQ_LIMIT	268000
 
 struct cpufreq_limit_handle *cpufreq_limit_get(unsigned long min_freq,
