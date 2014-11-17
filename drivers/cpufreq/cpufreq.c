61086a6f4554b660d436c1b71f056363e66e9ba1
TwistedUmbrella <twistedumbrella@gmail.com>
2014-11-17 18:38:03 -0500

    cpu: restore original version and disable per-cpu

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -338,16 +338,20 @@ extern void update_scaling_limits(unsigned int freq_min, unsigned int freq_max)
 	struct cpufreq_policy *policy, new_policy;
 
 	for_each_possible_cpu(cpu) {
-      if (hardlimit_user_enforced_status() == HARDLIMIT_USER_DISABLED) {
-          return;
-      }
+        if (hardlimit_user_enforced_status() == HARDLIMIT_USER_DISABLED) {
+            return;
+        }
         policy = cpufreq_cpu_get(cpu);
         if (!cpu_online(policy->cpu))
             return;
-        ret = cpufreq_get_policy(&new_policy, policy->cpu);
 		if (policy != NULL) {
-            policy->user_policy.min = new_policy.min = new_policy.user_policy.min = freq_min;
-            policy->user_policy.max = new_policy.max = new_policy.user_policy.max = freq_max;
+            ret = cpufreq_get_policy(&new_policy, policy->cpu);
+            policy->user_policy.min = new_policy.min = freq_min;
+            policy->user_policy.max = new_policy.max = freq_max;
+            if (hardlimit_user_enforced_status() == HARDLIMIT_USER_ENFORCED) {
+                new_policy.user_policy.min = freq_min;
+                new_policy.user_policy.max = freq_max;
+            }
 //          ret = cpufreq_set_policy(policy, &new_policy);
 		}
 	}
