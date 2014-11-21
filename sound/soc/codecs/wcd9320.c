644888e3f306eeb833da913a3f3e1cfa25c2710e
TwistedUmbrella <twistedumbrella@gmail.com>
2014-11-21 13:29:26 -0500

    Sound Control: Cumulative update from 3.0 to 3.6
    Sound Control: (Optional) work around for Nexus 4/5 audio issues
    
    (Use this only for devices with audio reset issues)
    
    Also bump version to 3.1
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    wcd9xxx-core: add register write without mutex protection
    
    This is assuming the calling function will take care of the mutex.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: (OPTIONAL) add power amp registers access control as well
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    sound control: add register cache
    
    bump to version 3.2
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    sound control: Samsung Microphone compatibility fixup
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: expose direct register manipulations to userspace
    
    bump driver version to 3.3
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: Misc clean up for newer WCD9xxx SOCs
    
    Bump driver version to 3.4
    
    Change-Id: Idaf374634368e61ba1fc9e24d0c5e1a994271400
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: let register lock be dependent on different HW revisions
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: separate locking mechanism for recording and playback
    
    with newer hardware revisions coming from Qualcomm, single register lock
    control isn't sufficient to cover both playback and recording usage
    scenarios
    
    bump to version 3.5
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Sound Control: add line gain to cached and register lock
    
    bump version to 3.6
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    * Undo swapping of taiko read & write in wcd9320

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -4189,6 +4189,12 @@ static int taiko_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 	return 0;
 }
 
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+extern int snd_hax_reg_access(unsigned int);
+extern unsigned int snd_hax_cache_read(unsigned int);
+extern void snd_hax_cache_write(unsigned int, unsigned int);
+#endif
+
 #ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL
 static
 #endif
@@ -4196,6 +4202,10 @@ int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
 	int ret;
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	int val;
+#endif
+
 	struct wcd9xxx *wcd9xxx = codec->control_data;
 
 	if (reg == SND_SOC_NOPM)
@@ -4210,7 +4220,19 @@ int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 				reg, ret);
 	}
 
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	if (!snd_hax_reg_access(reg)) {
+		if (!((val = snd_hax_cache_read(reg)) != -1)) {
+			val = wcd9xxx_reg_read_safe(codec->control_data, reg);
+		}
+	} else {
+		snd_hax_cache_write(reg, value);
+		val = value;
+	}
+	return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, val);
+#else
 	return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, value);
+#endif
 }
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 EXPORT_SYMBOL(taiko_write);
@@ -6958,6 +6980,8 @@ static struct regulator *taiko_codec_find_regulator(struct snd_soc_codec *codec,
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 struct snd_soc_codec *fauxsound_codec_ptr;
 EXPORT_SYMBOL(fauxsound_codec_ptr);
+int wcd9xxx_hw_revision;
+EXPORT_SYMBOL(wcd9xxx_hw_revision);
 #endif
 
 static int taiko_codec_probe(struct snd_soc_codec *codec)
@@ -6981,6 +7005,12 @@ static int taiko_codec_probe(struct snd_soc_codec *codec)
 	codec->control_data = dev_get_drvdata(codec->dev->parent);
 	control = codec->control_data;
 
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	if (TAIKO_IS_1_0(control->version))
+		wcd9xxx_hw_revision = 1;
+	else
+		wcd9xxx_hw_revision = 2;
+#endif
 	wcd9xxx_ssr_register(control, taiko_device_down,
 			     taiko_post_reset_cb, (void *)codec);
 
