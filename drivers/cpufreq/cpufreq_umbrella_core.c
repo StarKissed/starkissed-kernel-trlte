6cd5d6eda97bca0f1251f2a162573e6fa815bfb1
TwistedUmbrella <twistedumbrella@gmail.com>
2014-12-04 22:35:44 -0500

    umbrella_core: don't skip waking up speedchange_task if target_freq > policy->cur

--- drivers/cpufreq/cpufreq_umbrella_core.c
+++ drivers/cpufreq/cpufreq_umbrella_core.c
@@ -731,7 +731,8 @@ static void cpufreq_umbrella_core_timer(unsigned long data)
 		pcpu->floor_validate_time = now;
 	}
 
-	if (pcpu->target_freq == new_freq) {
+    if (pcpu->target_freq == new_freq &&
+        pcpu->target_freq <= pcpu->policy->cur) {
 		trace_cpufreq_umbrella_core_already(
 			data, cpu_load, pcpu->target_freq,
 			pcpu->policy->cur, new_freq);
